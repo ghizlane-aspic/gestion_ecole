75896c6a0c86b2b54d47a39207c69ba43f4e7c25
chaimae <kshchaimae@gmail.com>
2025-10-17 12:49:09 +0200

    sécurisation

--- Gestion_Actions/login.php
+++ Gestion_Actions/login.php
@@ -1,3 +1,21 @@
 <?php
-// Gestion_Actions\login.php
+require_once '../Acces_BD/login.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = htmlspecialchars($_POST['email']);
+    $password = $_POST['password'];
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die("Format d'email invalide !");
+    }
+
+    if (login($email, $password) === false) {
+        echo "<p style='color:red;'>Identifiants incorrects.</p>";
+        echo "<a href='../index.php'>Retour</a>";
+    }
+}
+
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+    logout();
+}
+?>
